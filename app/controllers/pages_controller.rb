0f58abbc994a5913e9ed4e9b3ede62a55e6e6159
Paulo DAlberti <phdalberti@gmail.com>
2022-02-12 15:02:12 +0900

    Basic display of spells in view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
   def home
+    # return @spells = [] if params[:q].blank?
+
+    @spells = Spell.first(10)
   end
 end
