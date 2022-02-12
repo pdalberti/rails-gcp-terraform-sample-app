23301fb30851817cb89845875926594bc337d14a
Paulo DAlberti <phdalberti@gmail.com>
2022-02-12 14:41:04 +0900

    Created spell sanitizer service

---
+++ app/services/spell_sanitizer_service.rb
@@ -0,0 +1,80 @@
+class SpellSanitizerService < ApplicationService
+  attr_reader :un_spell
+  attr_accessor :attributes
+
+  def initialize(unsanitized_spell)
+    @un_spell =   unsanitized_spell.split("\n\n")
+    @attributes = { description: unsanitized_spell }
+  end
+
+  def call
+    assign_name
+    assign_school_level_and_ritual
+    assign_casting
+    assign_range
+    assign_components
+    assign_duration_and_concentration
+    Spell.create!(attributes)
+  end
+
+  private
+
+  def assign_name
+    # "ANTIMAGICKÉ POLE"
+    attributes[:name] = un_spell.first
+  end
+
+  def assign_school_level_and_ritual
+    # "*Vyvolávání 6. úrovně* (rituál)"
+    words = un_spell[1].split(' ')
+    attributes[:school] = words.first[1..]
+    attributes[:level] = words[1].to_i
+    attributes[:ritual] = words.last == '(rituál)'
+  end
+
+  def assign_casting
+    # "**Vyvolání:** 1 akce"
+    attributes[:casting] = non_bold_text(un_spell[2])
+  end
+
+  def assign_range
+    # "**Dosah:** Ty sám (koule o poloměru 2 sáhy)"
+    attributes[:range] = non_bold_text(un_spell[3])
+  end
+
+  def assign_components
+    # "**Složky:** P. S (inkoust na bázi olova v hodnotě aspoň 10 zl, který kouzlo spotřebuje)"
+    ############ REWORK using N-N table
+    # components = non_bold_text(un_spell[4])
+    # attributes[:components] = if components.include?('(')
+    #                 text = text_between_parentheses(components)
+    #                 if text.include?(' zl')
+    #                   value = extract_value(text, text.index(' zl'))
+    #                   "#{components.split('(').first}(#{value}#{text.include?('aspoň') ? '+' : ''} zl)"
+    #                 else
+    #                   components.split(' (').first
+    #                 end
+    #               else
+    #                 components
+    #               end
+  end
+
+  def assign_duration_and_concentration
+    # "**Trvání:** Soustředění, až 1 hodina"
+    duration = non_bold_text(un_spell[5]).split(' ')
+    concentration = duration.first == 'Soustředění,'
+    attributes[:concentration] = concentration
+    attributes[:duration] = concentration ? duration[1..].join(' ').capitalize : duration.join(' ')
+  end
+
+  # helper methods
+  ########################################
+
+  def non_bold_text(string)
+    string.split('** ').last
+  end
+
+  # def text_between_parentheses(string)
+  #   string[/\((.*?)\)/m, 1]
+  # end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-    require 'database_cleaner/active_record'
+require 'database_cleaner/active_record'
+require_relative '../app/services/application_service'
+require_relative '../app/services/spell_sanitizer_service'
 
-    DatabaseCleaner.allow_production = true
-    DatabaseCleaner.allow_remote_database_url = true
-    DatabaseCleaner.strategy = :truncation
-    DatabaseCleaner.clean
+DatabaseCleaner.allow_production = true
+DatabaseCleaner.allow_remote_database_url = true
+DatabaseCleaner.strategy = :truncation
+DatabaseCleaner.clean
 
-    # write your new seeds after this line
+# write your new seeds after this line
 
+f = File.open('storage/11-kapitola-a.md').read
+f = f.split('### ')[1..] # split by spell title and select only spells
+f.each { |spell| SpellSanitizerService.call(spell) }
+
+puts "Created #{Spell.count} spells"
