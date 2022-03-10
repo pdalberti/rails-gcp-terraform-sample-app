0d90a4d3621f2a814ad261fdb84feeb168d75768
Paulo DAlberti <phdalberti@gmail.com>
2022-03-10 23:35:43 +0900

    column rollback

--- db/migrate/20220220043247_change_columns_to_enum.rb
+++ db/migrate/20220220043247_change_columns_to_enum.rb
@@ -1,9 +1,9 @@
 class ChangeColumnsToEnum < ActiveRecord::Migration[6.1]
   def change
     change_table :spells do |t|
-      t.remove :casting
-      t.remove :range
-      t.remove :duration
+      t.remove :casting, type: :string
+      t.remove :range, type: :string
+      t.remove :duration, type: :string
 
       t.integer :casting
       t.integer :range
