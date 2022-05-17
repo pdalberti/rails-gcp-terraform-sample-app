531ed557be68fe606307338840aa026533e3dc0a
Paulo DAlberti <phdalberti@gmail.com>
2022-05-18 08:57:59 +0900

    Fix rutial and concentration

--- app/controllers/dnd_controller.rb
+++ app/controllers/dnd_controller.rb
@@ -1,6 +1,12 @@
 class DndController < ApplicationController
   def kouzla
-    @q = Spell.ransack(params[:q])
+    @q = Spell.ransack(spell_params)
     @spells = @q.result
+  end
+
+  private
+
+  def spell_params
+    params[:q]&.reject { |_k, v| v == '0' }
   end
 end
