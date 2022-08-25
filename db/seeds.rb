1c7b9261bf814efde05b8fc3c92a5c99c034fb8a
Paulo DAlberti <phdalberti@gmail.com>
2022-08-25 16:13:13 +0900

    Refactored seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,31 +17,17 @@ end
 puts "Created #{Spell.count} DnD spells"
 
 
-File.read('storage/dnd-items.md').gsub("\r", '').split("<dnd-item\n").each do |unsanitized_item|
-  next if unsanitized_item.blank?
+{
+  'dnd-items' => { split: 'dnd-item', class: 'Dnd::Item' },
+  'fate-stunts' => { split: 'stunt', class: 'Fate::Trick' },
+  'drd2-zs' => { split: 'drd2-zs', class: 'Drd2::Ability' }
+}.each do |path, values|
+  File.read("storage/#{path}.md").gsub("\r", '').split("<#{values[:split]}\n").each do |unsanitized_item|
+    next if unsanitized_item.blank?
 
-  unsanitized_item = unsanitized_item.gsub('</dnd-item>', '').rstrip
-  Dnd::ItemSanitizerService.call(unsanitized_item)
+    unsanitized_item = unsanitized_item.gsub("</#{values[:split]}>", '').rstrip
+    "#{values[:class]}SanitizerService".constantize.call(unsanitized_item)
+  end
+
+  puts "Created #{values[:class].constantize.count} #{values[:class].gsub('::', ' ').pluralize}"
 end
-
-puts "Created #{Dnd::Item.count} DnD items"
-
-
-File.read('storage/fate-stunts.md').split("<stunt\n").each do |unsanitized_stunt|
-  next if unsanitized_stunt.blank?
-
-  unsanitized_stunt = unsanitized_stunt.gsub('</stunt>', '').rstrip
-  Fate::TrickSanitizerService.call(unsanitized_stunt)
-end
-
-puts "Created #{Fate::Trick.count} Fate stunts"
-
-
-File.read('storage/drd2-zs.md').gsub("\r", '').split("<drd2-zs\n").each do |unsanitized_ability|
-  next if unsanitized_ability.blank?
-
-  unsanitized_ability = unsanitized_ability.gsub('</drd2-zs>', '').rstrip
-  Drd2::AbilitySanitizerService.call(unsanitized_ability)
-end
-
-puts "Created #{Drd2::Ability.count} DrD2 abilities"
