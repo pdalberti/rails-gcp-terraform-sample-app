96c1802493b21d21bffdaaae611afae8b495baca
Paulo DAlberti <phdalberti@gmail.com>
2022-08-28 21:17:00 +0900

    contributors

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -6,4 +6,11 @@ module PagesHelper
   def collator
     ::TwitterCldr::Collation::Collator.new(:cz)
   end
+
+  def readable_contributors(contributors)
+    return contributors.first if contributors.length == 1
+    return contributors.join(' a ') if contributors.length == 2
+
+    "#{contributors[0..-2].join(', ')} a #{contributors.last}"
+  end
 end
