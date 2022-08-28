111ff9ed5c9815a4985d1329c9694a3bf5d16a76
Paulo DAlberti <phdalberti@gmail.com>
2022-08-28 12:41:58 +0900

    Add sorting to drd2 abilities

--- app/models/drd2/ability.rb
+++ app/models/drd2/ability.rb
@@ -22,6 +22,18 @@
 #  updated_at      :datetime         not null
 #
 class Drd2::Ability < ApplicationRecord
-  scope :sort_by_czech_name_asc, -> { sort { |a, b| Spell.collator.compare(a.name, b.name) } }
-  scope :sort_by_czech_name_desc, -> { sort { |a, b| Spell.collator.compare(b.name, a.name) } }
+  scope :sort_by_czech_name_asc, -> { sort { |a, b| Drd2::Ability.collator.compare(a.name, b.name) } }
+  scope :sort_by_czech_name_desc, -> { sort { |a, b| Drd2::Ability.collator.compare(b.name, a.name) } }
+  scope :sort_by_attribute_and_name_asc, lambda {
+    sort do |a, b|
+      [Drd2::Ability.collator.get_sort_key(a.drd2_attribute.first), Drd2::Ability.collator.get_sort_key(a.name)] <=>
+        [Drd2::Ability.collator.get_sort_key(b.drd2_attribute.first), Drd2::Ability.collator.get_sort_key(b.name)]
+    end
+  }
+  scope :sort_by_attribute_and_name_desc, lambda {
+    sort do |a, b|
+      [Drd2::Ability.collator.get_sort_key(b.drd2_attribute.first), Drd2::Ability.collator.get_sort_key(a.name)] <=>
+        [Drd2::Ability.collator.get_sort_key(a.drd2_attribute.first), Drd2::Ability.collator.get_sort_key(b.name)]
+    end
+  }
 end
